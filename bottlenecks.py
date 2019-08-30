a4685de71ecdf4569495602977711a2e09f63ba4
purvimalik18 <purvi.malik_cs17@gla.ac.in>
2019-08-30 17:44:01 +0530

    completed bottle_necks problem solution

---
+++ bottlenecks.py
@@ -0,0 +1,11 @@
+n = int(input())
+m = 0
+c = 0
+res = list(map(int, input().split(' ')))
+for i in range(n):
+    if m < res[i]:
+        m = res[i]
+for i in range(n):
+    if m == res[i]:
+        c = c+1
+print(c)
